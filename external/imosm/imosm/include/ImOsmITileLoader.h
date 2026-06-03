e6eec5a3404118ac02b71f92ecc9bf8dd29178e8
securit-lol <evdokim131@gmail.com>
2026-06-03 14:58:11 +0300

    test

--- external/imosm/imosm/include/ImOsmITileLoader.h
+++ external/imosm/imosm/include/ImOsmITileLoader.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <vector>
+#include <memory>
+
 namespace ImOsm {
 using ImTextureID = void *;
+class ITile;
 
 class ITileLoader {
 public:
@@ -10,6 +14,8 @@ public:
   virtual void beginLoad(int z, int xmin, int xmax, int ymin, int ymax) {}
   virtual ImTextureID tileAt(int z, int x, int y) = 0;
   virtual int tileCount() const { return 0; };
+  // Return reference to currently loaded tiles (may be empty)
+  virtual const std::vector<std::shared_ptr<ITile>>& getTiles() const = 0;
   virtual void endLoad() {}
 };
 } // namespace ImOsm
